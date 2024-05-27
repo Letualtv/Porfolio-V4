9b07be91db97234c5ac5c14823d71e91527c070e
Antonio P. <letual@outlook.es>
2024-05-27 16:50:28 +0200

    Poroflio

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const HeroSection = () => {
+  return (
+    <section>
+    
+    <h1 className='text-white'>
+
+        Hola, soy Antonio
+
+    </h1>
+    
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import headerPic from "../assets/Bosquito.jpg";
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12">
+
+<h1 className="text">Hola, soy Antonio</h1>
+
+    <HeroSection />
+     
+    </main>
+  );
+}
+
