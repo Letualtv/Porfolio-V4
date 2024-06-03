0590151ae1e5a77216e28dde3086ba0537cef906
Antonio P. <letual@outlook.es>
2024-06-03 18:18:42 +0200

    First next-react-ts-tailwind project

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <section>
-    
-    <h1 className='text-white'>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hola, soy Antonio{" "}
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="col-span-5">
+        <Image
+      src="/assets/Bosquito.jpg"
+      alt="Foto de Bosco"
+      width={200}
+      height={100}
+    />
 
-        Hola, soy Antonio
-
-    </h1>
-    
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
